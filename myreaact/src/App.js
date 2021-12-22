b77c58610b841c38e895adfcb3595e9f574eaabd
1486218133 <1486218133@qq.com>
2021-12-22 22:01:57 +0800

    logo

---
+++ myreaact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './images/logo.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo'>
+        <img src={logo} alt=''/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
